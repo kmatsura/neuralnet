db1197e893cc653bdfd710dffd2646d6d870d420
kmatsuuraHMC <bcdearth@gmail.com>
2020-04-27 06:12:22 +0900

    環境、ディレクトリ整備

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# neural_network
-The package of computing neural network
+This software is released under the MIT License, see LICENSE.txt.
+
+This package is now under development.
+
+# Description
+This is the package of to compute neural network.
+
+# How to install
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nueral_network',
+    version='0.0.1',
+    description='compute neural network',
+    author='Kengo Matsuura',
+    author_email='kengo11141996(at)gmail.com',  # convert (at) to @
+    url='https://github.com/kmatsuuraHMC/neural_network',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
